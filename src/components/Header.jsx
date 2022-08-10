bac8984154d503d910577e5303ddb55bacc27a1c
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-08-10 11:19:36 -0700

    create pages files and calendar app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Header = () => {
-  return <div></div>;
+const Header = ({ category, title }) => {
+  return (
+    <div className="mb-10">
+      <div className="text-gray-400">
+        <p>{category}</p>
+        <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+          {title}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
